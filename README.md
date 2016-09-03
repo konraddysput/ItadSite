20ca209d8834a36be9e0fb11b40d4d7d2fc175c5
Mersey <konrad.dysput@gmail.com>
2016-09-03 15:57:23 +0200

    Initial commit - basic styles, basic configuration. Temporary template and theme

---
+++ README.md
@@ -0,0 +1,33 @@
+## Andantino game - front-end project
+Front-end web application for andantino game. This is only aa user interface to andantino game, so if you would like to play vs AI you have to download a C++ Andantino API (https://github.com/konraddysput/Andantino-Api)
+
+###Author:
+Konrad Dysput
+
+###Technologies:
+* JavaScript
+* TypeScript
+* jQuery
+* SASS
+* CSS
+* HTML
+* gulp
+
+###How to run?
+Open file index.html
+Warning :
+if you would like to play vs AI download a C++ API available here: https://github.com/konraddysput/Andantino-Api
+
+###Requirements
+* gulp* 
+* SASS compiller*
+* typescript compiler*
+* webbrowser
+
+###Licence
+MIT
+
+###Contact
+For more information contact me via e-mail:
+* konrad.dysput@gmail.com
+
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp'), 				// importing gulp package
+	uglify =require('gulp-uglify'),			//importing gulp uglify to minify javascrit[]
+	sass = require('gulp-ruby-sass'),		// importing gulp sass compiler
+	plumber = require('gulp-plumber'),
+	livereload = require('gulp-livereload'),
+	concat = require('gulp-concat');
+
+//concat js
+gulp.task('scripts', function(){
+	  return gulp.src(['build/scripts/jquery-1.10.2.min.js','build/scripts/!(jquery-1.10.2.min.js)*.js'])
+    		.pipe(concat('site.js'))
+		    .pipe(gulp.dest('scripts'));
+});
+
+//Styles task
+//Uglify sass to css
+gulp.task('custom-styles', function(){
+	 return sass('build/styles/*.scss',{
+	 	style:'compressed'
+	 })
+	.pipe(plumber())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('build/styles'))
+    .pipe(livereload());
+});
+
+//concat css files
+gulp.task('styles', function(){
+	 return gulp.src('build/styles/*.css')
+    		.pipe(concat('site.css'))
+		    .pipe(gulp.dest('styles'));
+	 	// gulp.src('build/styles/*.css')				//load files
+		// .pipe(plumber())
+		// .pipe(gulp.dest('build/styles'))		//we saving them - output
+		// .pipe(livereload()); 	
+
+});
+//uglify fonts
+gulp.task('fonts', function(){
+	 	gulp.src('build/fonts/*.css')				//load files
+		.pipe(plumber())
+		.pipe(gulp.dest('build/styles'))		//we saving them - output
+		.pipe(livereload()); 	
+
+});
+
+
+//Watch task
+//JS watch
+//SCSS watch
+gulp.task('watch', function(){
+	var server = livereload();
+
+	gulp.watch('build/scripts/*.js', ['scripts']);
+	gulp.watch('build/styles/*.scss',['custom-styles']);
+})
+
+
+
+
+gulp.task('default', ['scripts','custom-styles','fonts','styles','watch']); //default - task name, array is a task array that have to be minified
+
